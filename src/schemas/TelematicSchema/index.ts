fc38765e3efbc85e86e056801922a4e7dc4a8157
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2021-08-20 09:58:23 +0200

    fix: added spacings, corrected equipment header key

--- src/schemas/TelematicSchema/index.ts
+++ src/schemas/TelematicSchema/index.ts
@@ -12,29 +12,36 @@ export interface TelematicDocument extends Document {
     SerialNumber: string;
     SnapshotTime: string;
   };
+
   Location: {
     Latitude: number;
     Longitude: number;
     Altitude: number;
     AltitudeUnits: Distance;
   };
+
   CumulativeIdleHours: {
     Hour: number;
   };
+
   CumulativeOperatingHours: {
     Hour: number;
   };
+
   Distance: {
     OdometerUnits: Distance;
     Odometer: number;
   };
+
   EngineStatus: {
     Running: boolean;
   };
+
   FuelUsed: {
     FuelUnits: Volume;
     FuelConsumed: number;
   };
+
   FuelRemaining: {
     Percent: number;
   };
@@ -43,7 +50,7 @@ export interface TelematicDocument extends Document {
 export const TelematicSchema = decorateSchema(
   new Schema(
     {
-      equipmentHeader: {
+      EquipmentHeader: {
         OEMName: { type: String },
         Model: { type: String },
         SerialNumber: { type: String },
@@ -56,23 +63,29 @@ export const TelematicSchema = decorateSchema(
         Altitude: { type: Number },
         AltitudeUnits: { type: String },
       },
+
       CumulativeIdleHours: {
         Hour: { type: Number },
       },
+
       CumulativeOperatingHours: {
         Hour: { type: Number },
       },
+
       Distance: {
         OdometerUnits: { type: String },
         Odometer: { type: Number },
       },
+
       EngineStatus: {
         Running: { type: Boolean },
       },
+
       FuelUsed: {
         FuelUnits: { type: String },
         FuelConsumed: { type: Number },
       },
+
       FuelRemaining: {
         Percent: { type: Number },
       },
